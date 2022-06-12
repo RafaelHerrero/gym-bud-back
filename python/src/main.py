ec18185494cc49ae6404caa3af021d1ff0872a7c
Rafael Herrero <rafaelgherrero@gmail.com>
2022-06-12 10:18:11 -0300

    {ReqFromWeb} Add middleware allow methods

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import users_routes
 
 app = FastAPI(title="Gym-Bud-back App")
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     users_routes.router,
