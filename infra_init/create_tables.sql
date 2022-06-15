6a37fa32b1a190cb3a9668a78dbece7036047397
Rafael Herrero <rafaelgherrero@gmail.com>
2022-06-15 00:53:45 -0300

    create mock data

--- infra_init/create_tables.sql
+++ infra_init/create_tables.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE public.users (
-	user_id TEXT NOT NULL,
+	user_id TEXT UNIQUE NOT NULL,
 	user_firstname text NOT NULL,
 	user_lastname text NOT NULL,
 	user_login TEXT NOT NULL,
@@ -10,71 +10,77 @@ CREATE TABLE public.users (
 	primary key(user_login)
 );
 
--- CREATE TABLE public.workouts (
---     workout_id TEXT NOT NULL,
---     workout_name TEXT NOT NULL,
---     workout_description TEXT,
--- 	created_at timestamp with time zone,
--- 	updated_at timestamp with time zone DEFAULT now(),
---     primary key(workout_id)
--- );
+CREATE TABLE public.exercise_plans (
+    exercise_plan_id TEXT UNIQUE NOT NULL,
+    exercise_plan_name TEXT NOT NULL,
+    exercise_plan_description TEXT,
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone DEFAULT now(),
+    primary key(exercise_plan_id)
+);
+
+CREATE TABLE public.workouts (
+    workout_id TEXT UNIQUE NOT NULL,
+    workout_name TEXT NOT NULL,
+    workout_description TEXT,
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone DEFAULT now(),
+    primary key(workout_id)
+);
+
+CREATE TABLE public.exercises (
+    exercise_id TEXT UNIQUE NOT NULL,
+    exercise_name TEXT NOT NULL,
+	exercise_muscle_group TEXT,
+    exercise_description TEXT,
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone DEFAULT now(),
+    primary key(exercise_id)
+);
 
 
--- CREATE TABLE public.exercises (
---     exercise_id TEXT NOT NULL,
---     exercise_name TEXT NOT NULL,
---     exercise_description TEXT,
--- 	created_at timestamp with time zone,
--- 	updated_at timestamp with time zone DEFAULT now(),
---     primary key(exercise_id)
--- );
+CREATE TABLE public.user_exercise_plans (
+    user_exercise_plan_id TEXT UNIQUE NOT NULL,
+    user_id TEXT REFERENCES public.users(user_id) NOT NULL,
+    exercise_plan_id TEXT REFERENCES public.exercise_plans(exercise_plan_id) NOT NULL,
+	exercise_plan_started_at timestamp with time zone,
+	exercise_plan_finished_at timestamp with time zone,
+	exercise_plan_expected_duration TEXT,
+	created_at timestamp with time zone DEFAULT now(),
+	updated_at timestamp with time zone DEFAULT now(),
+    primary key(user_exercise_plan_id)
+);
 
--- CREATE TABLE public.users_workouts (
---     user_workout_id TEXT NOT NULL,
---     user_id TEXT references public.users(user_id),
---     workout_id TEXT references public.workouts(workout_id),
--- 	created_at timestamp with time zone,
--- 	updated_at timestamp with time zone DEFAULT now(),
---     primary key(user_workout_id)
--- );
+CREATE TABLE public.exercise_plan_workouts (
+    exercise_plan_workout_id TEXT UNIQUE NOT NULL,
+    workout_id TEXT references public.workouts(workout_id),
+	exercise_plan_id TEXT references public.exercise_plans(exercise_plan_id),
+	created_at timestamp with time zone,
+	updated_at timestamp with time zone DEFAULT now(),
+    primary key(exercise_plan_workout_id)
+);
 
--- CREATE TABLE public.users_workout_periodizacao (
---     workout_periodizacao_id TEXT NOT NULL,
---     periodizacao_name TEXT NOT NULL,
---     user_id TEXT references public.users(user_id),
---     workout_id TEXT references public.workouts(workout_id),
--- 	created_at timestamp with time zone,
--- 	updated_at timestamp with time zone DEFAULT now(),
---     primary key(workout_periodizacao_id)
--- );
+CREATE TABLE public.workout_exercises (
+    workout_exercise_id TEXT UNIQUE NOT NULL,
+    workout_id TEXT references public.workouts(workout_id),
+    exercise_id TEXT references public.exercises(exercise_id),
+	exercise_sets TEXT,
+    exercise_reps TEXT,
+	exercise_description TEXT,
+	created_at timestamp with time zone,
+	updated_at timestamp with time zone DEFAULT now(),
+    primary key (workout_exercise_id)
+);
 
--- CREATE TABLE public.workouts_exercises (
---     workout_exercise_id TEXT NOT NULL,
---     workout_id TEXT references public.workouts(workout_id),
---     exercise_id TEXT references public.exercises(exercise_id),
---     default_reps INT,
--- 	created_at timestamp with time zone,
--- 	updated_at timestamp with time zone DEFAULT now(),
---     primary key (workout_exercise_id)
--- );
+CREATE TABLE public.workout_session (
+    workout_session_id TEXT NOT NULL,
+    user_exercise_plan_id TEXT references public.user_exercise_plans(user_exercise_plan_id),
+    exercise_plan_workout_id TEXT references public.exercise_plan_workouts(exercise_plan_workout_id),
+	workout_exercise_id TEXT references public.workout_exercises(workout_exercise_id),
+	workout_session_sets TEXT,
+	workout_session_reps TEXT,
+	created_at timestamp with time zone,
+	updated_at timestamp with time zone DEFAULT now(),
+    primary key( workout_session_id)
+);
 
--- CREATE TABLE public.workout_session (
---     workout_session_id TEXT NOT NULL,
---     user_id TEXT references public.users(user_id),
---     workout_id TEXT references public.workouts(workout_id),
---     session_start timestamp with time zone,
---     session_end timestamp with time zone,
--- 	created_at timestamp with time zone,
--- 	updated_at timestamp with time zone DEFAULT now(),
---     primary key( workout_session_id)
--- );
-
--- CREATE TABLE public.users_workouts_exercises (
---     user_workout_exercise_id TEXT NOT NULL,
---     user_workout_id TEXT references public.users_workouts(user_workout_id),
---     exercise_id TEXT references public.exercises(exercise_id),
---     number_of_reps_executed INT,
--- 	created_at timestamp with time zone,
--- 	updated_at timestamp with time zone DEFAULT now(),
---     primary key(user_workout_exercise_id)
--- );
--- infra_init/fill_tables.sql
+++ infra_init/fill_tables.sql
@@ -1,45 +1,75 @@
-INSERT INTO public.users (user_id,user_firstname,user_lastname,user_login,user_password,created_at) VALUES ('1AGTW234', 'Eloy', 'Herrero', 'eloy', 'senha_eloy', '2022-06-08');
-INSERT INTO public.users (user_id,user_firstname,user_lastname,user_login,user_password,created_at) VALUES ('ASD1F2FA', 'Rafael', 'Herrero', 'rafael', 'senha_rafa', '2022-05-18');
-INSERT INTO public.users (user_id,user_firstname,user_lastname,user_login,user_password,created_at) VALUES ('BKSUFHE3', 'Thales', 'Fernandes', 'thales', 'senha_thales', '2022-06-11');
+INSERT INTO public.users (user_id,user_firstname,user_lastname,user_login,user_password) VALUES ('1AGTW234', 'Eloy', 'Herrero', 'eloy', 'c91644671c970ffbd9cb0f05881203b5d88430edff78820c0e340d2eff0a3a6a');
+INSERT INTO public.users (user_id,user_firstname,user_lastname,user_login,user_password) VALUES ('ASD1F2FA', 'Rafael', 'admin', 'admin', '66ec97cbb01eafa9114570058be56287f37d426b82db1b4e75efb23473838621');
+INSERT INTO public.users (user_id,user_firstname,user_lastname,user_login,user_password) VALUES ('BKSUFHE3', 'Thales', 'Fernandes', 'thales', 'senha_thales');
 
--- INSERT INTO public.workouts (workout_id,workout_name,workout_description,created_at) VALUES ('WORKOUT01', 'Treino A', 'treino para realizar no primeiro dia de treino da semana', '2000-01-01');
+INSERT INTO public.exercise_plans (exercise_plan_id,exercise_plan_name,exercise_plan_description) VALUES ('FORCA', 'Treino de Força', 'Estamos na fase de Força da nossa periodização, vamos fazer poucas repetições e muito peso');
+INSERT INTO public.exercise_plans (exercise_plan_id,exercise_plan_name,exercise_plan_description) VALUES ('RESISTENCIA', 'Treino de Resistencia', 'Vamos realizar muitas repetições e pouca carga para desenvolver resistencia');
 
--- INSERT INTO public.exercises (exercise_id,exercise_name,exercise_description,created_at) VALUES ('AGACHA_ID', 'Agachamento Livre', 'Não usar almofadinha', '2023-09-05');
--- INSERT INTO public.exercises (exercise_id,exercise_name,exercise_description,created_at) VALUES ('SUPINO_ID', 'Supino', 'Supino de segunda', '2022-06-11');
--- INSERT INTO public.exercises (exercise_id,exercise_name,exercise_description,created_at) VALUES ('REMADA_ID', 'Remada Curvada', 'Não usar o remo', '2022-06-11');
+INSERT INTO public.workouts (workout_id,workout_name,workout_description) VALUES ('WORKOUT01', 'Treino A', 'Full Upper Body');
+INSERT INTO public.workouts (workout_id,workout_name,workout_description) VALUES ('WORKOUT02', 'Treino B', 'Full Lower Body');
+INSERT INTO public.workouts (workout_id,workout_name,workout_description) VALUES ('WORKOUT03', 'Treino C', 'Full Upper Body 2');
+INSERT INTO public.workouts (workout_id,workout_name,workout_description) VALUES ('WORKOUT04', 'Treino D', 'Full Body');
 
--- INSERT INTO public.users_workouts (user_workout_id,user_id,workout_id,created_at) VALUES ('RAFA_TREINO A', 'ASD1F2FA', 'WORKOUT01', '2022-01-01', '1999-03-05');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('AGACHA_ID', 'Agachamento Livre', 'Legs', 'Não usar almofadinha');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('SUPINO_INCLINADO_SMITH_ID', 'Supino inclinado no Smith', 'Chest', 'banco 45');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('SUPINO_RETO_ARTICULADO_ID', 'Supino reto vertical', 'Chest', 'preferencia articulado');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('CROSS_OVER_ID', 'Cross over polia alta', 'Chest', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('DESENVOLVIMENTO_ID', 'Desenvolvimento máquina pegada pronada', 'Shoulder', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('ELEV_LAT_ID', 'Elevação lateral com halteres', 'Shoulder', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('ELEV_LAT_MAQ_ID', 'Elevação lateral na máquina', 'Shoulder', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('REMADA_MAQUINA_ID', 'Remada máquina articulada pegada pronada', 'Back', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('REMADA_BAIXA_ID', 'Remada baixa com triângulo', 'Back', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('PUXADA_FRENTE_ID', 'Puxada frente pegada pronada', 'Back', '');
 
--- INSERT INTO public.users_workout_periodizacao (workout_periodizacao_id ,periodizacao_id,user_id,workout_id,created_at) VALUES ('RAFA_FORCA_TREINO A', 'Forca', 'ASD1F2FA', 'WORKOUT01', '2022-06-11');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('CADEIRA_ABT_ID', 'Cadeira abdutora', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('CADEIRA_FLEX_ID', 'Cadeira flexora', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('LEG_45_ID', 'Leg 45', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('AGACHA_HACK_ID', 'Agachamento Hack trilhos', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('CADEIRA_EXT_ID', 'Cadeira extensora', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('CADEIRA_ADU_ID', 'Cadeira adutora', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('PANTU_ID', 'Panturrilha em pé', 'Legs', '');
 
--- INSERT INTO periodizacao (periodizacao_id, periodizacao_name, created_at) VALUES ()
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('CRUCIFIXO_ID', 'Crucifixo reto máquina', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('SUPINO_INCLINADO_HALT_ID', 'Supino inclinado com halteres', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('PULLOVER_HALT_ID', 'Pullover com halter', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('REMADA_CURVADA_BARRA_ID', 'Remada curvada com barra reta', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('REMADA_CAVALINHO_ID', 'Remada cavalinho com apoio de tronco pegada neutra', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('DESENVOLV_HALT_ID', 'Desenvolvimento com halteres', 'Legs', 'banco 70');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('TRICEPS_PARALELA_ID', 'Tríceps na paralela máquina', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('ROSCA_DIRETA_ID', 'Rosca direta com halteres ', 'Legs', 'supinado – sem rotação');
 
--- INSERT INTO public.workouts_exercises (workout_exercise_id,workout_id,exercise_id,default_reps,created_at) VALUES ('WORKOUT01_AGACHA', 'WORKOUT01', 'AGACHA_ID', 10, '2022-06-10' );
--- INSERT INTO public.workouts_exercises (workout_exercise_id,workout_id,exercise_id,default_reps,created_at) VALUES ('WORKOUT01_SUPINO', 'WORKOUT01', 'SUPINO_ID', 12, '2022-06-10' );
--- INSERT INTO public.workouts_exercises (workout_exercise_id,workout_id,exercise_id,default_reps,created_at) VALUES ('WORKOUT01_REMADA', 'WORKOUT01', 'REMADA_ID', 15, '2022-06-10' );
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('MESA_FLEX_ID', 'Mesa flexora', 'Legs', 'ou livre caso não tenha acesso');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('ELEV_PELV_ID', 'Elevação pélvica máquina', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('PANTURRILHA_SENTADO_ID', 'Panturrilha sentado', 'Legs', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('PUXADA_FRENTE_TRIANG_ID', 'Puxada frente com triângulo', 'Back', '');
+INSERT INTO public.exercises (exercise_id,exercise_name,exercise_muscle_group,exercise_description) VALUES ('ELEV_LAT_UNI_ID', 'Elevação lateral unilateral na polia baixa', 'Legs', 'Shoulder');
 
--- INSERT INTO public.workouts_exercises_series (workout_exercise_series_id, workout_id, exercise_id, series_id, series_number, created_at) VALUES ('WORKOUT01_TREINOO A', 'WORKOUT01', 'AGACHA_ID', 'SERIES3', 3, '2022-05-01');
--- INSERT INTO public.workouts_exercises_series (workout_exercise_series_id, workout_id, exercise_id, series_id, series_number, created_at) VALUES ('WORKOUT01_TREINOO A', 'WORKOUT01', 'AGACHA_ID', 'SERIES3', 3, '2022-05-01');
+INSERT INTO public.user_exercise_plans (user_exercise_plan_id,user_id,exercise_plan_id,exercise_plan_started_at,exercise_plan_finished_at,exercise_plan_expected_duration) VALUES ('RAFA_FORCA', 'ASD1F2FA', 'FORCA', '2022-01-01', '2022-02-01', '8');
+INSERT INTO public.user_exercise_plans (user_exercise_plan_id,user_id,exercise_plan_id,exercise_plan_started_at,exercise_plan_finished_at,exercise_plan_expected_duration) VALUES ('RAFA_RESIST', 'ASD1F2FA', 'RESISTENCIA', '2021-11-01', '2021-12-02', '10');
 
--- INSERT INTO public.workouts_exercises_reps (workout_exercise_reps_id, workoiut_id, exercide_id, reps_id, reps_number, created_at) VALES ();
+INSERT INTO public.exercise_plan_workouts (exercise_plan_workout_id,exercise_plan_id,workout_id) VALUES ('FORCA_WORKOUT01', 'FORCA', 'WORKOUT01');
+INSERT INTO public.exercise_plan_workouts (exercise_plan_workout_id,exercise_plan_id,workout_id) VALUES ('FORCA_WORKOUT02', 'FORCA', 'WORKOUT02');
+INSERT INTO public.exercise_plan_workouts (exercise_plan_workout_id,exercise_plan_id,workout_id) VALUES ('FORCA_WORKOUT03', 'FORCA', 'WORKOUT03');
+INSERT INTO public.exercise_plan_workouts (exercise_plan_workout_id,exercise_plan_id,workout_id) VALUES ('FORCA_WORKOUT04', 'FORCA', 'WORKOUT04');
 
--- INSERT INTO public.workouts_exercides_desc (workout_exercise_desc_id, workout_id, exercise_id, desc_id, description_field, created_at) VALUES ();
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT01_SUPINO_INCLINADO_SMITH_ID', 'WORKOUT01', 'SUPINO_INCLINADO_SMITH_ID', '1', '', 'MR');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT01_SUPINO_RETO_ARTICULADO_ID', 'WORKOUT01', 'SUPINO_RETO_ARTICULADO_ID', '1', '', 'RP');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT01_CROSS_OVER_ID', 'WORKOUT01', 'CROSS_OVER_ID', '1-1', '', 'WS-BO');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT01_DESENVOLVIMENTO_ID', 'WORKOUT01', 'DESENVOLVIMENTO_ID', '1', '', 'MR');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT01_ELEV_LAT_ID', 'WORKOUT01', 'ELEV_LAT_ID', '1', '', 'RP');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT01_ELEV_LAT_MAQ_ID', 'WORKOUT01', 'ELEV_LAT_MAQ_ID', '1-1', '', 'WS-BO');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT01_REMADA_MAQUINA_ID', 'WORKOUT01', 'REMADA_MAQUINA_ID', '1', '', 'RP');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT01_REMADA_BAIXA_ID', 'WORKOUT01', 'REMADA_BAIXA_ID', '1', '', 'MR');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT01_PUXADA_FRENTE_ID', 'WORKOUT01', 'PUXADA_FRENTE_ID', '1-1', '', 'WS-BO');
 
--- INSERT INTO public.seres (series_id, series_number, created_at) VALUES ();
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT02_CADEIRA_ABT_ID', 'WORKOUT02', 'CADEIRA_ABT_ID', '1', '', 'MR');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT02_CADEIRA_FLEX_ID', 'WORKOUT02', 'CADEIRA_FLEX_ID', '1', '', 'MR');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT02_AGACHA_ID', 'WORKOUT02', 'AGACHA_ID', '1-1', '', 'WS-BO');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT02_LEG_45_ID', 'WORKOUT02', 'LEG_45_ID', '1', '', 'MR');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT02_AGACHA_HACK_ID', 'WORKOUT02', 'AGACHA_HACK_ID', '1', '', 'RP');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT02_CADEIRA_EXT_ID', 'WORKOUT02', 'CADEIRA_EXT_ID', '1', '', 'MR');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT02_CADEIRA_ADU_ID', 'WORKOUT02', 'CADEIRA_ADU_ID', '1', '', 'MR');
+INSERT INTO public.workout_exercises (workout_exercise_id,workout_id,exercise_id,exercise_sets,exercise_reps,exercise_description) VALUES ('WORKOUT02_PANTU_ID', 'WORKOUT02', 'PANTU_ID', '1', '', 'RP');
 
--- INSERT INTO public.reps (reps_id, reps_number, created_at) VALUES ();
+-- INSERT INTO public.workout_session (workout_session_id,user_exercise_plan_id,exercise_plan_workout_id,workout_exercise_id,workout_session_sets,workout_session_reps) VALUES ();
 
--- INSERT INTO public.exercise_description (description_id, description_name, description_filed, created_at) VALUES ();
-
-
-
-
--- INSERT INTO public.workout_session (workout_session_id,user_id,workout_id,session_start,session_end,created_at) VALUES ();
-
--- INSERT INTO public.users_workouts_exercises (user_workout_exercise_id,user_workout_id,exercise_id,number_of_reps_executed,created_at) VALUES ();
-
-
--- -> SUPINO 3 SÉRIES DE 10 REPETIÇÕES
--- -> AGACHAMENDO 1 SÉRIE DE TIPO_DE_SERIE (LOADING_SET (3 SÉRIES DE 8 REP))
-
--- FORCA 
