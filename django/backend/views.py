dd4ce3c66ce9bb03f22d075e0341852f2a24fc0d
Rafael Herrero <rafaelgherrero@gmail.com>
2023-07-25 11:39:21 -0300

    auth

--- django/backend/views.py
+++ django/backend/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
-
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework.decorators import action
 from rest_framework import viewsets
@@ -16,10 +16,12 @@ def index(request):
     return HttpResponse("Hello, world. You're at the GymBud Backend index.")
 
 class ExerciseViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticated]
     queryset = Exercise.objects.all()
     serializer_class = ExerciseSerializer
 
 class WorkoutViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticated]
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
 
@@ -44,21 +46,26 @@ class WorkoutViewSet(viewsets.ModelViewSet):
 
 
 class WorkoutExerciseViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticated]
     queryset = WorkoutExercise.objects.all()
     serializer_class = WorkoutExerciseSerializer
 
 class WorkoutPlanViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticated]
     queryset = WorkoutPlan.objects.all()
     serializer_class = WorkoutPlanSerializer
 
 class UserWorkoutPlanViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticated]
     queryset = UserWorkoutPlan.objects.all()
     serializer_class = UserWorkoutPlanSerializer
 
 class WorkoutPlanWorkoutViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticated]
     queryset = WorkoutPlanWorkout.objects.all()
     serializer_class = WorkoutPlanWorkoutSerializer
 
 class WorkoutSessionViewSet(viewsets.ModelViewSet):
+    # permission_classes = [IsAuthenticated]
     queryset = WorkoutSession.objects.all()
     serializer_class = WorkoutSessionSerializer
